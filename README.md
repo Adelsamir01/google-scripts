92260571c20e77b08633a304f8df87a3bfdb184b
Adel Samir <a.elzemity@nu.edu.eg>
2020-06-12 02:31:29 +0200

    adding emailing on sheet edits script

---
+++ README.md
@@ -0,0 +1,3 @@
+# Google Scripts by Adel
+## This repo contains some cool and practical scripts created to automate the pooring stuff.
+
---
+++ emailing-on-edit-google-sheets.js
@@ -0,0 +1,39 @@
+function onEdit(e){
+NewOrderEmail(e);
+}
+
+
+function NewOrderEmail(e){
+  let taletoon = "asktaletoon@gmail.com";
+  let adel = "adelelzemity@gmail.com";
+  let yasser = "ay47757@gmail.com";
+  let eshiba = "mohamedeshiba08@gmail.com";
+  let msg = "NEW ORDER WITH ID #";
+  let body = "\n\n Check out the new order details from here:\n\nhttps://bit.ly/3dWy2zX";
+  //selecting the active sheet
+  var activeSheet = e.source.getActiveSheet();
+  
+    //selecting the sheet by name
+  if(activeSheet.getName() == 'New Order'){
+    var cell = e.range;
+    var col = cell.getColumn();
+    
+    //selecting the column
+    if(col == 16){
+      
+      //testing cell value
+      if(cell.getValue() == 'confirmed'){
+       
+        //the action done if true
+        var orderid = activeSheet.getRange(cell.getRow(),1).getValue();
+        var clientname = activeSheet.getRange(cell.getRow(),2).getValue();
+        msg = msg+orderid+" FOR "+clientname;
+               //sending emails
+        MailApp.sendEmail(adel,msg,"Hey Adel,"+body);
+        MailApp.sendEmail(eshiba,msg,"Hey Eshiba,"+body);
+        MailApp.sendEmail(yasser,msg,"Hey Yasser,"+body);
+      
+      }
+    }
+  }
+}
